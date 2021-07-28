fed091121bcbfb4502ca138c3c5eced138e9d751
Normar Macawile <nmacawile@gmail.com>
2021-07-28 14:56:22 +0800

    Add Clients controller create route

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,8 +1,19 @@
 class ClientsController < ApplicationController
   respond_to :json
-  
+
   def index
     @clients = Client.all
     json_response @clients
+  end
+
+  def create
+    @client = Client.create!(client_params)
+    head :created
+  end
+
+  private
+
+  def client_params
+    params.require(:client).permit(:name, :address)
   end
 end
--- spec/requests/clients_spec.rb
+++ spec/requests/clients_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe "Clients API", type: :request do
   let(:headers) { authorized_request_headers(user.id) }
   let!(:clients) { create_list :client, 10 }
   let(:client_ids) { clients.map { |c| c.id.to_s } }
+  let(:new_client) { build :client }
 
   describe "GET /clients" do
     before do
@@ -18,6 +19,27 @@ RSpec.describe "Clients API", type: :request do
 
     it 'returns status code 200' do
       expect(response).to have_http_status 200
+    end
+  end
+
+  describe 'POST /clients' do
+    before do
+      post '/clients',
+           params: {
+             client: {
+               name: new_client.name,
+               address: new_client.address
+             }
+           }.to_json,
+           headers: headers
+    end
+
+    it 'increases the templates count by 1' do
+      expect(Client.count).to eq 11
+    end
+
+    it 'returns status code 201' do
+      expect(response).to have_http_status 201
     end
   end
 end
