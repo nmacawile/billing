f8846e6217057a1d8aca4fe3cab1b3824586bf94
Normar Macawile <nmacawile@gmail.com>
2021-08-25 18:32:28 +0800

    Add Department model

---
+++ src/app/models/department.ts
@@ -0,0 +1,12 @@
+import { DepartmentItemParams, DepartmentItem } from './department-item';
+import { MongoidId } from './mongoid-id';
+
+export interface DepartmentParams {
+  name: string;
+  department_items?: DepartmentItemParams[];
+}
+
+export interface Department extends MongoidId {
+  name: string;
+  department_items?: DepartmentItem[];
+}
