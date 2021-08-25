554c6bc75781dab749518180a7d9a48acacfc95d
Normar Macawile <nmacawile@gmail.com>
2021-08-25 18:32:13 +0800

    Add DepartmentItem model

---
+++ src/app/models/department-item.ts
@@ -0,0 +1,27 @@
+import { MongoidId } from './mongoid-id';
+
+interface Deduction {
+  mon?: number;
+  tue?: number;
+  wed?: number;
+  thu?: number;
+  fri?: number;
+  sat?: number;
+  sun?: number;
+}
+
+interface DepartmentItemPartials {
+  days: string;
+  deduction?: Deduction;
+  price: number;
+  quantity: number;
+  position: number;
+}
+
+export interface DepartmentItemParams extends DepartmentItemPartials {
+  item: string;
+}
+
+export interface DepartmentItem extends DepartmentItemPartials, MongoidId {
+  item: { $oid: string };
+}
