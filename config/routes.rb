85c2211751e5233edf54c2e791cb24ec024e9245
Normar Macawile <nmacawile@gmail.com>
2021-08-30 10:29:56 +0800

    Clean up unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   post 'users', to: 'users#create'
   post 'auth/login', to: 'authentication#authenticate'
-  resources :templates do
-    resources :departments
+  resources :templates, except: [:new, :edit] do
+    resources :departments, except: [:new, :edit]
   end
-  resources :items
+  resources :items, except: [:new, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
