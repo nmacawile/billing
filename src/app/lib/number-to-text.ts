f5eee3c0f702fde782844eacb4d00080cd07cb0f
Normar Macawile <nmacawile@gmail.com>
2022-05-10 13:12:22 +0800

    Fix number to text function rounding up values

--- src/app/lib/number-to-text.ts
+++ src/app/lib/number-to-text.ts
@@ -1,7 +1,7 @@
 const writtenNumber = require('written-number');
 
 export function numberToText(n: number): string {
-  let text = writtenNumber(n, { noAnd: true });
+  let text = writtenNumber(Math.floor(n), { noAnd: true });
   const cents = (100 * (n % 1)).toFixed();
   if (+cents > 0) text += ' and ' + cents + '/100';
   return ('the sum of ' + text + ' pesos only.').toUpperCase();
